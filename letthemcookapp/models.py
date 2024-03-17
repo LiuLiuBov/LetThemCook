4ae1f99412151f94b023c48ddf5961fad706f106
Liubov Poliakova <pluba5835@gmail.com>
2024-03-17 16:08:42 +0000

    improved models.py

--- letthemcookapp/models.py
+++ letthemcookapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Recipe(models.Model):
+    
     title = models.CharField(max_length=255)
     quick_description = models.TextField()
     content = models.TextField()
@@ -12,12 +14,30 @@ class Recipe(models.Model):
     def __str__(self):
         return self.title
 
+class Save(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    saved_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # user cannot save the same recipe more than once.
+        unique_together = ('user', 'recipe')
+
+    def __str__(self):
+        return f"{self.user.username} saved {self.recipe.title}"
+
 class Review(models.Model):
-    content = models.TextField()
-    rating = models.IntegerField()
+
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # restrict users from reviewing the same recipe multiple times.
+        unique_together = ('user', 'recipe')
 
     def __str__(self):
         return f"Review by {self.user.username} for {self.recipe.title}"
-
