fbadf4acd6f7fe6e331dd18dad0e76a68bdc2072
aidanling65 <2763417l@student.gla.ac.uk>
2024-03-20 15:58:46 +0000

    Review submission implemented

--- letthemcookapp/views.py
+++ letthemcookapp/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect,render
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+import numpy as np
 
 from .models import Review, User, Recipe
-from .forms import RecipeForm
-from django.contrib.auth.decorators import login_required
+from .forms import RecipeForm,ReviewForm
 
 def index_view(request):
     recipes = Recipe.objects.all()
@@ -63,14 +62,59 @@ def register(request):
 
 @login_required
 def createrecipe(request):
-    recipes = Recipe.objects.all()
-    return render(request, 'createrecipe.html', {'recipes': recipes})
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.user = request.user
+            recipe.save()
+            return redirect('index')
+    else:
+        form = RecipeForm()
+    return render(request, 'createrecipe.html', {'form': form})
+
+@login_required
+def create_review(request, recipe_id):
+    print(recipe_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            review = Review(recipe=Recipe.objects.get(id=recipe_id), user=request.user, rating=form.cleaned_data["rating"])
+            review.comment = form.cleaned_data["comment"]
+            review.save()
+            return redirect('index')
+    else:
+        form = ReviewForm()
+    
+    return redirect('index')
 
 def saved(request):
     recipes = Recipe.objects.all()
     return render(request, 'saved.html', {'recipes': recipes})
 
-@login_required
 def profile(request):
     recipes = Recipe.objects.all()
     return render(request, 'profile.html', {'recipes': recipes})
+    
+def recipe(request, recipe_id):
+    context_dict = {}
+    
+    try:
+        recipe = Recipe.objects.get(id=recipe_id)
+        context_dict['recipe'] = recipe
+        context_dict['ingredients'] = recipe.ingredients.split('\n')
+        context_dict['form'] = ReviewForm()
+        
+        reviews = Review.objects.filter(recipe=recipe_id)
+        context_dict['reviews'] = reviews
+        
+        sum = np.sum(review.rating for review in reviews)
+        mean = round(sum / len(reviews),2)
+        context_dict['average'] = mean
+
+    except Recipe.DoesNotExist:
+        context_dict['recipe'] = None
+        return redirect(reverse('letthemcook:index'))
+    
+    return render(request, 'recipe.html', context=context_dict)
+        
