d843ba6cf116c926d101274bf69ee48d6b523734
Camilla Cummins <2796631C@student.gla.ac.uk>
2024-03-21 18:08:38 +0000

    model tests

--- letthemcookapp/tests.py
+++ letthemcookapp/tests.py
@@ -1,19 +1,21 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Recipe, Review, Save
+from django.urls import reverse
 
-# Create your tests here.
+# MODEL TESTS
 
 class RecipeModelTest(TestCase):
 
     #tests 'average_rating' field of recipe is updated
 
     def test_update_average(self):
-        user1 = User.objects.create_user(user='testuser', email='testuser@eg.com', password='testpassword')
-        user2 = User.objects.create_user(user='testuser2', email='testuser2@eg.com', password='testpassword2')
-        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients')
-        review1 = Review.objects.create(recipe=recipe, user=user1, rating=5, comment='very good')
-        review2 = Review.objects.create(recipe=recipe, user=user2, rating=4, comment='good')
+        user1 = User.objects.create_user(username='testuser', email='testuser@eg.com', password='testpassword')
+        user2 = User.objects.create_user(username='testuser2', email='testuser2@eg.com', password='testpassword2')
+        user3 = User.objects.create_user(username='testuser3', email='testuser3@eg.com', password='testpassword3')
+        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients', user=user1)
+        review1 = Review.objects.create(recipe=recipe, user=user2, rating=5, comment='very good')
+        review2 = Review.objects.create(recipe=recipe, user=user3, rating=4, comment='good')
 
         recipe.refresh_from_db()
         self.assertEqual(recipe.average_rating, 4.5)
@@ -21,13 +23,14 @@ class RecipeModelTest(TestCase):
     #test 'saves' field of recipe is updated when recipe is saved
 
     def test_update_saves(self):
-        user1 = User.objects.create_user(user='testuser', email='testuser@eg.com', password='testpassword')
-        user2 = User.objects.create_user(user='testuser2', email='testuser2@eg.com', password='testpassword2')
-        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients')
-        save1 = Save.objects.create(user=user1, recipe=recipe)
-        save2 = Save.objects.create(user=user2, recipe=recipe)
+        user1 = User.objects.create_user(username='testuser', email='testuser@eg.com', password='testpassword')
+        user2 = User.objects.create_user(username='testuser2', email='testuser2@eg.com', password='testpassword2')
+        user3 = User.objects.create_user(username='testuser3', email='testuser3@eg.com', password='testpassword3')
+        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients', user=user1)
+        save1 = Save.objects.create(user=user2, recipe=recipe)
+        save2 = Save.objects.create(user=user3, recipe=recipe)
 
-        recipe.refresh_from_db()
+        recipe.update_saves()
         self.assertEqual(recipe.saves, 2)
 
 class SaveModelTest(TestCase):
@@ -35,12 +38,13 @@ class SaveModelTest(TestCase):
     #tests a user cannot save the same recipe mutiple times
 
     def test_unique_together(self):
-        user1 = User.objects.create_user(user='testuser', email='testuser@eg.com', password='testpassword')
-        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients')
-        Save.objects.create(user=user1, recipe=recipe)
+        user1 = User.objects.create_user(username='testuser', email='testuser@eg.com', password='testpassword')
+        user2 = User.objects.create_user(username='testuser2', email='testuser2@eg.com', password='testpassword2')
+        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients', user= user1)
+        Save.objects.create(user=user2, recipe=recipe)
 
         with self.assertRaises(Exception):
-            Save.objects.create(user=user1, recipe=recipe)
+            Save.objects.create(user=user2, recipe=recipe)
         
 
 class ReviewModelTest(TestCase):
@@ -48,10 +52,13 @@ class ReviewModelTest(TestCase):
     #tests a user cannot review the same recipe mutiple times
 
     def test_unique_together(self):
-        user1 = User.objects.create_user(user='testuser', email='testuser@eg.com', password='testpassword')
-        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients')
-        Review.objects.create(user=user1, recipe=recipe)
+        user1 = User.objects.create_user(username='testuser', email='testuser@eg.com', password='testpassword')
+        user2 = User.objects.create_user(username='testuser2', email='testuser2@eg.com', password='testpassword2')
+        recipe = Recipe.objects.create(title='Test Recipe', quick_description='Test description', content='Test recipe content', ingredients='Test recipe ingredients', user=user1)
+        Review.objects.create(user=user2, recipe=recipe, rating=5, comment='amazing')
 
         with self.assertRaises(Exception):
-            Review.objects.create(user=user1, recipe=recipe)
+            Review.objects.create(user=user2, recipe=recipe, rating=2, comment='good')
 
+
+#VIEWS TESTS
